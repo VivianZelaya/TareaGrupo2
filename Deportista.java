931f15f5fd1489674e91a01166247c4e5bc6a481
MisaelP19 <127177526+MisaelP19@users.noreply.github.com>
2023-03-14 13:01:26 -0600

    Add files via upload

---
+++ Deportista.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea6;
+
+/**
+ *
+ * @author PEREIRA
+ */
+public class Deportista  extends Persona  {
+  
+    
+    public Deportista(){
+        //Establecer los métodos
+        setNombre("Misael Pereira");
+        setEdad(25);
+        setGenero("Masculino");
+    }
+
+}
