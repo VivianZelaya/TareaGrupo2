800d0a8b236241ffefe2509503e51e0ddf8a557b
MisaelP19 <127177526+MisaelP19@users.noreply.github.com>
2023-03-14 12:59:33 -0600

    Add files via upload

---
+++ Tarea6.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea6;
+
+/**
+ *
+ * @author PEREIRA
+ */
+public class Tarea6 {
+
+     public static void main(String[] args) {
+        
+        //Instanciar
+        Doctor Doc = new Doctor();
+        Deportista Dep = new Deportista();
+        
+        //Imprimir
+        Doc.imprimir();
+        System.out.println(" ");
+        Dep.imprimir();
+        
+    }
+}
+
+    
